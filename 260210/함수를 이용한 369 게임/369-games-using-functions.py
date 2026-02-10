0271af899775c674c46b2c56461e15e1e13ae74a
young010514 <young010514@naver.com>
2026-02-10 13:44:51 +0900

    Title: 함수를 이용한 369 게임, Time: 68ms, Memory: 17MB, Status: Wrong Answer - Codetree

--- 260210/함수를 이용한 369 게임/369-games-using-functions.py
+++ 260210/함수를 이용한 369 게임/369-games-using-functions.py
@@ -3,18 +3,13 @@ a, b = map(int, input().split())
 # Please write your code here.
 cnt =0
 def find(num):
-    
+    global cnt
     if num % 3 ==0 :
-        return 1
-    else : return 0
-def num369(num):
-    if num % 10 in [3,6,9]:
-        return 1
-    elif num //10 in [3,6,9]:
-        return 1
-    else:return 0
-    
-for i in range(a,b+1):
-    if find(i) or num369(i):
+        cnt +=1
+    elif num // 10 != 0 and (num //10) % 3 ==0:
         cnt += 1
+    elif  num % 3 !=0  and  (num % 10 ) % 3 == 0:
+        cnt += 1
+for i in range(a,b+1):
+    find(i)
 print(cnt)
